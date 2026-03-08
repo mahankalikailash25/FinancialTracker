2e5130fe7b5cd5fdfef4fef9a99bb640563d2797
mahankalikailash25 <mahankalikailash25@gmail.com>
2026-03-08 16:17:52 +0530

    Add files via upload

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+// Password : MoIVijGfKBMPVum6
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+import cors from 'cors';
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const mongoURI: string = "mongodb+srv://mahankalikailash25_db_user:MoIVijGfKBMPVum6@personalfinancetracker.oklzycc.mongodb.net/";
+
+mongoose.connect(mongoURI).then(() => console.log("CONNECTED TO MONGODB!")).catch((err) => console.error("Failed to connect to MongoDB: ", err));
+
+app.use("/financial-records", financialRecordRouter)
+
+app.listen(port, () => {
+    console.log(`Server Running on Port ${port}`);
+})
